b2642dcc54a29e0818766f8001eb01f6d0383a43
yu3589 <uegtz34714@yahoo.co.jp>
2025-07-31 21:12:11 +0900

    feat: Usersテーブルに外部キー(sweetness_type_id)を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_31_102049) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_31_120531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,8 +31,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_31_102049) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.bigint "sweetness_type_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["sweetness_type_id"], name: "index_users_on_sweetness_type_id"
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
+
+  add_foreign_key "users", "sweetness_types"
 end
