4c96a5a8e6919df708fdd2f12022585d010086f8
yu3589 <uegtz34714@yahoo.co.jp>
2025-08-01 11:04:38 +0900

    feat: SweetnessProfilesテーブルに外部キー(sweetness_type_id)を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_01_001450) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_01_003236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_01_001450) do
     t.integer "richness"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "sweetness_type_id", null: false
+    t.index ["sweetness_type_id"], name: "index_sweetness_profiles_on_sweetness_type_id"
     t.index ["user_id"], name: "index_sweetness_profiles_on_user_id"
   end
 
@@ -50,6 +52,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_01_001450) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "sweetness_profiles", "sweetness_types"
   add_foreign_key "sweetness_profiles", "users"
   add_foreign_key "users", "sweetness_types"
 end
