527162d5cba4833515a862c1cba8d6aaf2bce00b
yu3589 <uegtz34714@yahoo.co.jp>
2025-08-06 13:57:43 +0900

    feat: SweetnessProfilesテーブルにtokenカラムを追加

--- app/models/sweetness_profile.rb
+++ app/models/sweetness_profile.rb
@@ -1,4 +1,13 @@
 class SweetnessProfile < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :sweetness_type
+
+  before_create :generate_token
+
+  def generate_token
+    loop do
+      self.token = SecureRandom.uuid
+      break unless SweetnessProfile.exists?(token: token)
+    end
+  end
 end
