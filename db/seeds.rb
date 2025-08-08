d3583520bca55d63ad21fc5e4359d2cc711ea0cb
yu3589 <uegtz34714@yahoo.co.jp>
2025-08-09 05:56:59 +0900

    feat: categoryデータを保存

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+categories = [
+  { name: "チョコレート" },
+  { name: "グミ・ゼリー" },
+  { name: "クッキー・ビスケット" },
+  { name: "ケーキ・洋菓子" },
+  { name: "和菓子" },
+  { name: "アイス・冷菓" },
+  { name: "飲み物" },
+  { name: "キャンディ・あめ" },
+  { name: "ドーナツ" },
+  { name: "パン" },
+  { name: "スナック菓子" },
+  { name: "その他" }
+]
+
+categories.each do |cat|
+  Category.find_or_create_by!(name: cat[:name])
+end
