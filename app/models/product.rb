de549f4b9fd09745a4aed60ca4e741c442f11acf
yu3589 <uegtz34714@yahoo.co.jp>
2025-08-31 00:10:51 +0900

    feat:  商品詳細画面の作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ class Product < ApplicationRecord
   validates :manufacturer, presence: true
   validates :name, uniqueness: { scope: :manufacturer, message: "とメーカーの組み合わせは既に存在します" }
 
-
   belongs_to :category
   has_many :posts, dependent: :destroy
   has_one_attached :image
@@ -14,5 +13,16 @@ class Product < ApplicationRecord
 
   def perfect_sweetness_count
     posts.where(sweetness_rating: Post.sweetness_ratings[:perfect_sweetness]).size
+  end
+
+  def average_sweetness_scores
+    scores = PostSweetnessScore.where(post_id: posts.select(:id))
+    {
+      sweetness_strength: scores.average(:sweetness_strength),
+      aftertaste_clarity: scores.average(:aftertaste_clarity),
+      natural_sweetness: scores.average(:natural_sweetness),
+      coolness: scores.average(:coolness),
+      richness: scores.average(:richness)
+    }
   end
 end
