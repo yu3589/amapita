4190347aa4ce61297cd16c1b4367d7dc748cbe19
yu3589 <uegtz34714@yahoo.co.jp>
2025-09-03 23:58:42 +0900

    feat: 商品検索機能を実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,12 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all.order(:name)
   end
 
   def show
-    @category = Category.find(params[:id])
-    @products = @category.products.includes(:posts).order(created_at: :desc)
+    @category = Category.find_by!(slug: params[:slug])
+    # カテゴリ内の商品のみで検索
+    @q = @category.products.ransack(params[:q])
+    @products = @q.result(distinct: true).includes(:posts).order(created_at: :desc)
   end
 end
