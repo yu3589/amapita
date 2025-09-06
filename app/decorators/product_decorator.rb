6ecee6a6edadcf5720bed30adbdff35f1aa658dd
yu3589 <uegtz34714@yahoo.co.jp>
2025-09-06 21:38:20 +0900

    feat: User/Post/Product用Decorator追加、画像バリデーションモジュール作成

---
+++ app/decorators/product_decorator.rb
@@ -0,0 +1,16 @@
+class ProductDecorator < Draper::Decorator
+  delegate_all
+
+  COMMON_VARIANT_OPTIONS = { resize_to_fill: [ 400, 400 ], format: :webp, saver: { quality: 85 } }.freeze
+
+  def product_image(**options)
+    default_classes = "object-cover"
+    custom_classes  = options[:class]
+    merged_classes  = [ default_classes, custom_classes ].compact.join(" ")
+
+    h.image_tag(
+      object.image.attached? ? object.image.variant(COMMON_VARIANT_OPTIONS) : "default_image.png",
+      options.merge(class: merged_classes)
+    )
+  end
+end
---
+++ app/models/concerns/image_validatable.rb
@@ -0,0 +1,31 @@
+module ImageValidatable
+  extend ActiveSupport::Concern
+
+  included do
+    validate :image_type_and_size, if: -> { respond_to?(:image) && image.attached? }
+    validate :avatar_type_and_size, if: -> { respond_to?(:avatar) && avatar.attached? }
+  end
+
+  private
+
+  def image_type_and_size
+    validate_attachment(:image)
+  end
+
+  def avatar_type_and_size
+    validate_attachment(:avatar)
+  end
+
+  def validate_attachment(attachment_name)
+    attachment = send(attachment_name)
+    return unless attachment.attached?
+
+    unless attachment.content_type.in?(%w[image/png image/jpg image/jpeg])
+      errors.add(attachment_name, "はJPG・JPEG・PNG形式のファイルのみ対応しています")
+    end
+
+    if attachment.blob.byte_size > 5.megabytes
+      errors.add(attachment_name, "は5MB以下にしてください")
+    end
+  end
+end
