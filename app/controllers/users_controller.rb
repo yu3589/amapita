686436c9188711588b93fef1446e7381f3f8e663
yu3589 <uegtz34714@yahoo.co.jp>
2025-09-06 21:40:41 +0900

    feat: プロフィール画像のActive Storage対応、I18n化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts.order(created_at: :desc)
+    @user = User.find(params[:id]).decorate
+    @posts = @user.posts.order(created_at: :desc).decorate
     @sweetness_type = @user.sweetness_type
     @sweetness_profiles = @user.sweetness_profiles.last
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,21 @@
-class User < ApplicationRecord
-  belongs_to :sweetness_type, optional: true
-  has_many :sweetness_profiles, dependent: :destroy
-  has_many :posts
+require "open-uri"
 
-  validates :name, presence: true, length: { maximum: 10 }, uniqueness: true
+class User < ApplicationRecord
+  include ImageValidatable
+
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable,
+        :omniauthable, omniauth_providers: [ :google_oauth2 ]
+
+  has_one_attached :avatar
+
+  validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
   validates :uid, presence: true, uniqueness: { scope: :provider }
   validates :self_introduction, length: { maximum: 100 }
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [ :google_oauth2 ]
+  belongs_to :sweetness_type, optional: true
+  has_many :sweetness_profiles, dependent: :destroy
+  has_many :posts
 
   # providerとuidを使ってユーザーを検索、存在しなければ新規作成
   def self.from_omniauth(auth)
@@ -17,6 +23,11 @@ class User < ApplicationRecord
       user.name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
+
+      if auth.info.image.present?
+      downloaded_image = URI.open(auth.info.image)
+      user.avatar.attach(io: downloaded_image, filename: "#{user.name}_avatar.jpg")
+      end
     end
   end
 
@@ -30,5 +41,25 @@ class User < ApplicationRecord
 
   def self.ransackable_attributes(auth_object = nil)
     [ "name" ]
+  end
+
+    def google_avatar_url
+      # OAuth情報があればURLを返す
+      @google_avatar_url ||= begin
+      omniauth_data = self[:omniauth_data] # セッション等に保存しておく場合
+      omniauth_data[:info][:image] if omniauth_data
+    end
+  end
+
+  def profile_image_url
+    if avatar.attached?
+      # Active Storageの画像を優先
+      Rails.application.routes.url_helpers.url_for(avatar)
+    elsif google_avatar_url.present?
+      # Google認証で取得した画像URL
+      google_avatar_url
+    else
+      nil # デフォルト画像はDecoratorで処理
+    end
   end
 end
