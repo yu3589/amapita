d014fd4a6d5d2767f9d0b894a3955c6a34787800
yu3589 <uegtz34714@yahoo.co.jp>
2025-09-07 22:45:36 +0900

    docs: README更新およびスキーマ調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_04_031055) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_06_230550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,16 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_04_031055) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_bookmarks_on_product_id"
+    t.index ["user_id", "product_id"], name: "index_bookmarks_on_user_id_and_product_id", unique: true
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -126,6 +136,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_04_031055) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookmarks", "products"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "post_sweetness_scores", "posts"
   add_foreign_key "posts", "products"
   add_foreign_key "posts", "users"
