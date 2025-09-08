4de50f1901a5de1753e887d5b74adf044b0aed0f
yu3589 <uegtz34714@yahoo.co.jp>
2025-09-08 17:45:25 +0900

    feat: 管理画面ルーティングとi18nを設定

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,15 @@ Rails.application.routes.draw do
     resources :products, only: %i[show]
   end
 
+  namespace :admin do
+    root "dashboards#index"
+    resource :dashboard, only: %i[index]
+
+    get "login", to: "user_sessions#new"
+    post "login", to: "user_sessions#create"
+    delete "logout", to: "user_sessions#destroy"
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
