eb9b2d4d3f3acf378ea2236add2e6400bb29570c
yu3589 <uegtz34714@yahoo.co.jp>
2025-09-08 17:42:55 +0900

    feat: usersテーブルにroleカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_06_230550) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_08_020836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -128,8 +128,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_06_230550) do
     t.string "uid"
     t.bigint "sweetness_type_id"
     t.string "self_introduction"
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role"], name: "index_users_on_role"
     t.index ["sweetness_type_id"], name: "index_users_on_sweetness_type_id"
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
