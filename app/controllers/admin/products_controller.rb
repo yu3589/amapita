fc0945145a4d43c32bc4b5cc2a415cc90120f89d
yu3589 <uegtz34714@yahoo.co.jp>
2025-09-08 23:54:09 +0900

    feat: 商品管理画面のCRUD機能を実装

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,50 @@
+class Admin::ProductsController < Admin::BaseController
+  before_action :authenticate_user!
+
+  def index
+    @q = Product.ransack(params[:q])
+    @products = @q.result(distinct: true).all.order(created_at: :desc).decorate
+    @total_count = @products.count
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path, notice: t("defaults.flash_message.registered", item: Product.model_name.human)
+    else
+      flash.now[:alert] = t("defaults.flash_message.not_registered", item: Product.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice: t("defaults.flash_message.updated", item: Product.model_name.human)
+    else
+      flash.now[:alert] = t("defaults.flash_message.not_updated", item: Product.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.image.purge if @product.image.attached?
+    @product.destroy!
+    redirect_to admin_products_path, notice: t("defaults.flash_message.deleted", item: Product.model_name.human)
+  end
+
+    private
+
+  def product_params
+    params.require(:product).permit(:name, :manufacturer, :category_id, :image)
+  end
+end
