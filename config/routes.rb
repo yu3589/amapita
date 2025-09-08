1244ace4bd9fca35fff4a12d0ae69824adaaa22c
yu3589 <uegtz34714@yahoo.co.jp>
2025-09-08 23:53:43 +0900

    feat: 商品管理画面のルーティングとi18nを修正・追記

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :diagnoses, only: %i[new create]
   get "diagnoses/result/:token", to: "diagnoses#show", as: :diagnosis_result
 
-  resources :posts, only: %i[index show new create edit update destroy]
+  resources :posts
 
   resources :products, only: %i[index show]
 
@@ -26,11 +26,13 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "dashboards#index"
-    resource :dashboard, only: %i[index]
 
     get "login", to: "user_sessions#new"
     post "login", to: "user_sessions#create"
     delete "logout", to: "user_sessions#destroy"
+
+    resource :dashboard, only: %i[index]
+    resources :products
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
