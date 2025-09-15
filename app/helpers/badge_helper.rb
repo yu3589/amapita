9276aaa7d0cf9cef0f99bbd8b1e34fbd0adfdb6d
yu3589 <uegtz34714@yahoo.co.jp>
2025-09-15 22:39:00 +0900

    feat: 甘さツインバッジ表示のビューとアセット修正

--- app/helpers/badge_helper.rb
+++ app/helpers/badge_helper.rb
@@ -1,11 +1,15 @@
 module BadgeHelper
   def display_post_badge(user)
-    badge = PostBadge.current_post_badge(user)
-    badge&.name
+    PostBadge.current_post_badge(user)&.name
   end
 
   def display_sweetness_twin_badge(user)
-    badge = SweetnessTwinBadge.current_twin_badge(user)
+    badge = SweetnessTwins::Badge.current_twin_badge(user)
     badge&.name
+  end
+
+  # current_userから見てuserがツインか？
+  def sweetness_twin?(user)
+    SweetnessTwin.exists?(user: current_user, twin_user: user)
   end
 end
