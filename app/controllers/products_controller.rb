81a1b299880dbb1882e47795b9b81c305574c491
yu3589 <uegtz34714@yahoo.co.jp>
2025-09-17 18:17:57 +0900

    feat: 商品詳細ページで商品登録者の投稿画像を表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,8 @@ class ProductsController < ApplicationController
     @posts = @product.decorate.posts.order(created_at: :desc)
     @average_scores = @product.average_sweetness_scores
     @user_post = @posts.find_by(user_id: current_user.id) if user_signed_in?
+
+    @product_image_uploader = @product.posts.includes(:user).order(:created_at).first
+    @product_image_uploader_id = @product_image_uploader&.user_id
   end
 end
