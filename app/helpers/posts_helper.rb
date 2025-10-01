3447467937d3f3efeaf74d4265916d92bc8ca329
yu3589 <uegtz34714@yahoo.co.jp>
2025-10-02 00:13:50 +0900

    feat: 甘さツイン未存在時のメッセージとアイコン追加

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+  def recommend_state(user, recommended_posts)
+    return :not_diagnosed unless user.sweetness_profiles.any?
+
+    if user.sweetness_twins.present?
+      return :twin_with_posts if recommended_posts.any?
+      return :twin_without_posts
+    else
+      return :no_twin
+    end
+  end
 end
