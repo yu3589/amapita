ef1bbdef0fb4cd27b04ea5e2bcd2356135b39a19
yu3589 <uegtz34714@yahoo.co.jp>
2025-10-13 17:05:38 +0900

    feat: 独自ドメイン対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "www.amapita.com"
+  config.hosts << "amapita.com"
+  config.hosts << "amapita.onrender.com"
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
