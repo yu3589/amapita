d0f61283cb591abd1fbaa038bbf8eea38c320f50
yu3589 <uegtz34714@yahoo.co.jp>
2025-10-22 23:53:53 +0900

    feat: ローディング機能を実装

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -8,9 +8,16 @@ class PostDecorator < Draper::Decorator
     custom_classes  = options[:class]
     merged_classes  = [ default_classes, custom_classes ].compact.join(" ")
 
-    h.image_tag(
-      object.image.attached? ? object.image.variant(POST_VARIANT_OPTIONS) : "default_image.png",
-      options.merge(class: merged_classes)
-    )
+    image_source = if object.image.attached?
+      object.image.variant(POST_VARIANT_OPTIONS)
+    elsif object.product&.image&.attached?
+      object.product.image.variant(POST_VARIANT_OPTIONS)
+    elsif object.product&.product_image_url.present?
+      object.product.product_image_url
+    else
+      "default_image.png"
+    end
+
+    h.image_tag(image_source, options.merge(class: merged_classes))
   end
 end
