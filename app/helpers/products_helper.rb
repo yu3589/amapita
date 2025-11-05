ecd846b4cd4cc81fff0598617ffea620db386fb4
yu3589 <uegtz34714@yahoo.co.jp>
2025-11-06 06:09:24 +0900

    feat: 商品ページのナビゲーションを改善

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -8,4 +8,13 @@ module ProductsHelper
   def show_unpublish_post_link?(user_unpublish_post)
     user_unpublish_post.present?
   end
+
+  # 商品詳細の戻るボタン
+  def back_to_list_path(product)
+    if request.path.match?(/^\/categories\/[^\/]+\/products\/\d+/)
+      category_path(product.category.slug)
+    else
+      products_path
+    end
+  end
 end
